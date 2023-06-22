81eb0735477f46e8db6986878ceb7eb390b83c67
weesleycabral <weesleylima@hotmail.com>
2023-06-22 12:00:25 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
+import { JogoDaVelhaComponent } from './pages/jogo-da-velha/jogo-da-velha.component';
+import { HeaderComponent } from './pages/menu/header/header.component';
+import { FooterComponent } from './pages/menu/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalculadoraComponent,
+    JogoDaVelhaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
