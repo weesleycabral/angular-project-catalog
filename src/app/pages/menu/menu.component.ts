77a8112baa947a8a6703ec7fe8252fd6d167bb62
weesleycabral <weesleylima@hotmail.com>
2023-06-22 17:31:18 -0300

    lista de cards, titulos e descricao

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  public pages: number[] = [];
+  public titles: string[] = [];
+  public descricao: string[] = [];
+
+  constructor() {
+    this.pages = [1, 2, 3];
+    this.titles = ['Clima', 'Calculadora', 'Jogo da Velha'];
+    this.descricao = ['Digite o nome da cidade e consiga ver detalhes sobre seu clima', '#', '#'];
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
