75809e2d0a523fc01b61a4f39d156d445e0f1494
weesleycabral <weesleylima@hotmail.com>
2023-06-23 16:57:55 -0300

    ajeitando objeto cards, classes ativado/desativado cards

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from './models/card.model';
 
 @Component({
   selector: 'app-menu',
@@ -6,31 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  public pages: number[] = [];
-  public titles: string[] = [];
-  public descricao: string[] = [];
-  public rotas: string[] = [];
-  public cards;
+  // public pages: number[] = [];
+  // public titles: string[] = [];
+  // public descricao: string[] = [];
+  // public rotas: string[] = [];
+  public cards: Card[] = [];
 
   constructor() {
-    this.cards = {
-      pages: [1, 2, 3],
-      titles: ['Clima', 'Calculadora', 'Jogo da Velha'],
-      descricao: ['Digite o nome da cidade e consiga ver detalhes sobre seu clima',
-      'Faça cálculos que nem uma calculadora normal',
-      '#'
-    ],
-    rotas: ['weather', 'calculator'],
-    available: false
-    }
+    this.cards = [
+      {
+        name: 'Clima',
+        description: 'Digite o nome da cidade e consiga ver detalhes sobre seu clima',
+        route: 'weather',
+        available: true
+      },
+      {
+        name: 'Calculadora',
+        description: 'Faça cálculos que nem uma calculadora normal',
+        route: 'calculator',
+        available: true
+      },
+      {
+        name: 'Jogo da Velha',
+        description: 'Jogue um jogo da velha virtual',
+        route: '',
+        available: false
+      },
 
-    this.pages = [1, 2, 3];
-    this.titles = ['Clima', 'Calculadora', 'Jogo da Velha'];
-    this.descricao = ['Digite o nome da cidade e consiga ver detalhes sobre seu clima',
-      'Faça cálculos que nem uma calculadora normal',
-      '#'
-    ];
-    this.rotas = ['weather', 'calculator'];
+    ]
+
+    // this.pages = [1, 2, 3];
+    // this.titles = ['Clima', 'Calculadora', 'Jogo da Velha'];
+    // this.descricao = ['Digite o nome da cidade e consiga ver detalhes sobre seu clima',
+    //   'Faça cálculos que nem uma calculadora normal',
+    //   '#'
+    // ];
+    // this.rotas = ['weather', 'calculator'];
   }
 
   ngOnInit(): void {
---
+++ src/app/pages/menu/models/card.model.ts
@@ -0,0 +1,6 @@
+export interface Card {
+    name: string
+    description: string
+    route: string
+    available: boolean
+}
