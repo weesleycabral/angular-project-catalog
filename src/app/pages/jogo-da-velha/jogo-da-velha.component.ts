d78e482a4bf6314ec85670ffe7203f5dcd72f841
weesleycabral <weesleylima@hotmail.com>
2023-06-26 10:24:18 -0300

    mudando a estrutura dos squares, e clicando X e O funfando

--- src/app/pages/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/pages/jogo-da-velha/jogo-da-velha.component.ts
@@ -6,18 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jogo-da-velha.component.scss']
 })
 export class JogoDaVelhaComponent implements OnInit {
-public squares: any[] = Array(9);
-public player:string = 'X';
+public squares: any = []
+public player = 'X';
 public symbol: string[] = [];
+winningValues = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-constructor() {}
+constructor() {
+}
 
 ngOnInit(): void {
 
 }
 
-play(index: number) {
-  this.symbol[index] = this.player
+play(i: number) {
+    if (this.squares.length == 8) {
+    return console.log('deu velha')
+  }
+  console.log(this.squares)
+    this.squares.push({
+      jogada: this.player,
+      posicao: i
+    });
+
+
+    const quadrado = document.getElementById(`item-${i}`)
+quadrado!.innerHTML = this.player;
+this.player = this.player === 'X' ? 'O' : 'X';
+// this.checkWin(i)
+}
+
+checkWin(jogadaAtual: any) {
+  const currentPlay: any = [];
+  console.log(currentPlay);
+  this.squares.forEach((element: any) => {
+    if(element.jogada === jogadaAtual) {
+      currentPlay.push(element.posicao)
+    }
+  })
+  if(currentPlay.some(this.winningValues)) {
+    console.log('ganhou');
+  }
 }
 
 
